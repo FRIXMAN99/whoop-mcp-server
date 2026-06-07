07b84e5c1794b88f1744f967ab3a6f56e09351b6
FRIXMAN99 <marioafrixone@gmail.com>
2026-06-07 12:57:02 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -84,30 +84,34 @@ export interface WhoopSleep {
 }
 
 export interface WhoopWorkout {
-	id: string;
-	user_id: number;
-	created_at: string;
-	updated_at: string;
-	start: string;
-	end: string;
-	timezone_offset: string;
-	sport_id: number;
-	score_state: 'SCORED' | 'PENDING_SCORE' | 'UNSCORABLE';
-	score?: {
-		strain: number;
-		average_heart_rate: number;
-		max_heart_rate: number;
-		kilojoule: number;
-		percent_recorded: number;
-		zone_duration: {
-			zone_zero_milli: number;
-			zone_one_milli: number;
-			zone_two_milli: number;
-			zone_three_milli: number;
-			zone_four_milli: number;
-			zone_five_milli: number;
-		};
-	};
+  id: string;
+  user_id: number;
+  created_at: string;
+  updated_at: string;
+  start: string;
+  end: string;
+  timezone_offset: string;
+  sport_name: string;
+  sport_id?: number;
+  score_state: 'SCORED' | 'PENDING_SCORE' | 'UNSCORABLE';
+  score?: {
+    strain: number;
+    average_heart_rate: number;
+    max_heart_rate: number;
+    kilojoule: number;
+    percent_recorded: number;
+    distance_meter: number | null;
+    altitude_gain_meter: number | null;
+    altitude_change_meter: number | null;
+    zone_durations: {
+      zone_zero_milli: number;
+      zone_one_milli: number;
+      zone_two_milli: number;
+      zone_three_milli: number;
+      zone_four_milli: number;
+      zone_five_milli: number;
+    };
+  };
 }
 
 export interface WhoopPaginatedResponse<T> {
